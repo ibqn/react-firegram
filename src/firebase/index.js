a368947e9d1e3d95937d52c27d5837c9c764afb3
ibqn <evgen.ibqn@gmail.com>
2021-03-06 10:30:59 +0100

    initial commit

---
+++ src/firebase/index.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyAfH9Tsg5ffqXlEED3voRTwBQD9M0qxues',
+  authDomain: 'ibqn-firegram.firebaseapp.com',
+  projectId: 'ibqn-firegram',
+  storageBucket: 'ibqn-firegram.appspot.com',
+  messagingSenderId: '472151340470',
+  appId: '1:472151340470:web:c9feabc08096d884bb3d17',
+  measurementId: 'G-HN1L4G056F',
+}
+
+export default firebaseConfig
