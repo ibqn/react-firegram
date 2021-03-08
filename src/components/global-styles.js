bef18e633dac54effe05061e809c2e47263f8345
ibqn <evgen.ibqn@gmail.com>
2021-03-08 01:16:22 +0100

    Recommending against usage of css @import inside createGlobalStyle and what to do instead (#2997)

--- src/components/global-styles.js
+++ src/components/global-styles.js
@@ -3,8 +3,6 @@ import * as styled from 'styled-components'
 
 // const GlobalStyles = createGlobalStyle`
 const GlobalStyles = styled.createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Noto+Serif:wght@400;700&display=swap');
-
   :root {
     --primary: #efb6b2;
     --secondary: #4e4e4e;
